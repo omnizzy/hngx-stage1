507a54f8a35c3a726a6d4d0bbb37697ad9f1da5e
omnizzy <118431825+omnizzy@users.noreply.github.com>
2023-09-11 15:32:18 +0100

    Stage one commit

--- hngx1/src/main/java/com/example/hngx1/controller/InfoController.java
+++ hngx1/src/main/java/com/example/hngx1/controller/InfoController.java
@@ -1,4 +1,23 @@
 package com.example.hngx1.controller;
 
+import com.example.hngx1.service.InfoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
 public class InfoController {
+    private final InfoService infoService;
+
+    @GetMapping("/api")
+    public Map<String, Object> getInfo(
+            @RequestParam String slackName,
+            @RequestParam String track
+    ) {
+        return infoService.getInfo(slackName, track);
+    }
 }
